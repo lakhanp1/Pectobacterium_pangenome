18a0107225d2faca9a1ccf0db54e962396712e44
Lakhansing Pardeshi <lakhanp1@gmail.com>
2023-01-06 14:33:06 +0100

    merge genomes FASTA into single file

---
+++ scripts/a_preprocessing/a10_merge_genome_faa.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+# from pathlib import Path
+
+
+# argument parsing
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Merge all the FASTA sequences from genomes into single FASTA file",
+        prog="merge_genomes_faa"
+    )
+    parser.add_argument(
+        "--genomes-file",
+        help="Genome file with path to each genome on each line",
+        type=str, required=True
+    )
+
+    parser.add_argument(
+        "--out",
+        help=("Output file to store combined genomes FASTA records."
+              " Additionally, 'genome_chr_map.tab' file is created holding the"
+              " genome to chromosome mapping table."),
+        default="genomes_merged.fasta"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def merge_fasta(file_genomes, output):
+    logging.info(f'merging all genomes into {output}')
+    
+    file_chr_key = os.path.join(os.path.dirname(output), 'genome_chr_map.tab')
+    logging.info(f'storing genome to chr mapping in file {file_chr_key}')
+    
+    fa_writer = open(output, mode='w')
+    key_writer = open(file_chr_key, mode='w')
+
+    with open(file_genomes) as genomes:
+        for genome_id, genome_fa in enumerate(genomes, start=1):
+            logging.info(f'{genome_id} + {genome_fa.strip()}')
+            
+            with open(genome_fa.strip()) as fasta:
+                for line in fasta:
+                    
+                    if line.startswith('>'):
+                        # add genome id prefix to chr id
+                        new_header = f'{genome_id}_{line.strip()[1:]}'
+                        fa_writer.write(f'>{new_header}\n')
+                        key_writer.write(f'{genome_id}\t{new_header}\n')
+                        
+                    else:
+                        fa_writer.write(line)
+                    
+            
+    fa_writer.close()
+    key_writer.close()
+    
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    args = parse_args()
+    # print(args)
+    merge_fasta(file_genomes=args.genomes_file, output=args.out)
+
+
+if __name__ == "__main__":
+    main()
