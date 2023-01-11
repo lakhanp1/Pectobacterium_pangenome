c235746da44bf4c33545ecfa16b3c17e154185d5
Pardeshi, Lakhansing <lakhansing.pardeshi@wur.nl>
2023-01-11 18:34:46 +0100

    std visualization of trees with metadata

---
+++ scripts/c_analysis/c02_plot_evolutionary_trees.R
@@ -0,0 +1,215 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(configr))
+suppressPackageStartupMessages(library(ggpubr))
+suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(ggnewscale))
+suppressPackageStartupMessages(library(ape))
+suppressPackageStartupMessages(library(treeio))
+suppressPackageStartupMessages(library(ggtree))
+suppressPackageStartupMessages(library(ggtreeExtra))
+suppressPackageStartupMessages(library(ggstar))
+suppressPackageStartupMessages(library(viridisLite))
+
+## A script to visualize phylogenetic trees with some metadata
+
+rm(list = ls())
+
+source("https://raw.githubusercontent.com/lakhanp1/omics_utils/main/01_RScripts/02_R_utils.R")
+################################################################################
+set.seed(124)
+
+## argment parsing
+parser <- optparse::OptionParser()
+
+parser <- optparse::add_option(
+  parser, opt_str = c("-t", "--tree"), type = "character", action = "store",
+  help = "tree file in NEWICK format"
+)
+
+parser <- optparse::add_option(
+  parser, opt_str = c("-n", "--name"), type = "character", action = "store",
+  help = "name of the tree"
+)
+
+parser <- optparse::add_option(
+  parser, opt_str = c("-o", "--out"), type = "character", action = "store",
+  help = "output file prefix"
+)
+
+parser <- optparse::add_option(
+  parser, opt_str = c("-c", "--config"), type = "character", action = "store",
+  help = "project config YAML file"
+)
+
+opts <- optparse::parse_args(parser)
+
+################################################################################
+confs <- prefix_config_paths(
+  conf = suppressWarnings(configr::read.config(file = opts$config)),
+  dir = "."
+)
+
+# listviewer::jsonedit(confs)
+
+outDir <- confs$analysis$phylogeny$dir
+genus <- confs$genus
+
+outGroup <- confs$analysis$phylogeny$outgroup
+################################################################################
+
+genusPattern <- paste("(", genus, " )(?!sp\\.)", sep = "")
+
+sampleInfo <- suppressMessages(
+  readr::read_csv(file = confs$data$pangenomes$pectobacterium.v2$files$metadata)
+) %>% 
+  dplyr::mutate(
+    Genome = as.character(Genome),
+    SpeciesName = stringi::stri_replace(
+      str = SpeciesName, regex = genusPattern, replacement = "P. "
+    ),
+    SpeciesName = stringi::stri_replace(
+      str = SpeciesName, regex = "((\\w)[^ ]+ )((\\w)[^ ]+ )(subsp\\..*)",
+      replacement = "$2. $4. $5"
+    ),
+    nodeLabs = stringr::str_c(sampleName, " (", SpeciesName,")", sep = "")
+  ) %>% 
+  dplyr::select(id, everything())
+
+
+sampleInfoList <- dplyr::select(
+  sampleInfo, id, sampleName, SpeciesName, strain, nodeLabs, Genome
+) %>% 
+  purrr::transpose() %>% 
+  purrr::set_names(nm = purrr::map(., "id"))
+
+
+## read tree
+rawTree <- ape::read.tree(file = opts$tree)
+
+## set negative length edges => 0
+rawTree$edge.length[rawTree$edge.length < 0] <- 0
+
+## add data to tree
+treeTbl <- as_tibble(rawTree) %>%
+  dplyr::full_join(y = sampleInfo, by = c("label" = "Genome")) %>%
+  treeio::as.treedata() %>% 
+  treeio::root(outgroup = sampleInfoList[[outGroup]]$Genome, edgelabel = TRUE)
+
+################################################################################
+
+## function to add annotation to ggtree
+annotate_ggtree <- function(pt, offset){
+  pt2 <- pt +
+    ggtreeExtra::geom_fruit(
+      mapping = aes(starshape = type_material),
+      geom = "geom_star", fill = "#62BFED", size = 2, starstroke=0.1,
+      offset = offset, pwidth = 0.01
+    ) +
+    ggstar::scale_starshape_manual(
+      values = c("type strain" = 1)
+    ) + 
+    ggnewscale::new_scale_color() +
+    ## genome source
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, x = "source", color = source),
+      geom = "geom_point", shape = 15,
+      pwidth = 0.01
+    ) +
+    scale_color_manual(
+      values = c("NCBI" = "#406495", "WUR" = "#468e30", "NVWA" = "#ff6600")
+    ) +
+    ggnewscale::new_scale_fill() +
+    ## taxonomy check status
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, fill = taxonomy_check_status),
+      geom = "geom_star", starshape = 12, size = 2, color = alpha("white", 0),
+      pwidth = 0.01
+    ) +
+    scale_fill_manual(
+      values = c("Failed" = "red", "Inconclusive" = "blue", "OK" = alpha("white", 0))
+    ) +
+    ## collection year
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, label = collection_year),
+      geom = "geom_text", size = 3
+    ) +
+    ggnewscale::new_scale_color() +
+    ## country
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, label = geo_loc_country, color = geo_loc_country),
+      geom = "geom_text", size = 3, hjust = "left",
+      pwidth = 0.03
+    ) +
+    scale_color_manual(
+      values = c("Netherlands" = "#ff6600")
+    ) +
+    ggnewscale::new_scale_color() +
+    ## virulence PCR result
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, x = "vir_pcr", color = virulence_pcr),
+      geom = "geom_point",
+      pwidth = 0.01, offset = 0.05
+    ) +
+    scale_color_manual(
+      values = c("positive" = "red", "negative" = "green"),
+      na.value = alpha("white", 0)
+    ) +
+    ggnewscale::new_scale_color() +
+    ## virulence phenotype
+    ggtreeExtra::geom_fruit(
+      mapping = aes(y = id, x = "virulence", color = virulence),
+      geom = "geom_point", shape = 17, size = 2,
+      pwidth = 0.01, offset = 0.01
+    ) +
+    scale_color_manual(
+      values = c("virulent" = "red", "avirulent" = "green"),
+      na.value = alpha("white", 0)
+    )
+}
+
+################################################################################
+## plotting
+
+pt_tree <- ggtree::ggtree(tr = treeTbl) +
+  labs(title = "NJ tree")
+
+## mark outgroup
+pt_tree2 <- pt_tree + 
+  geom_point(
+    mapping = aes(shape = sampleName, color = sampleName),
+    size = 4
+  ) +
+  scale_shape_manual(name = "outgroup", values = setNames(16, outGroup)) +
+  scale_color_manual(name = "outgroup", values = setNames("red", outGroup)) +
+  ggnewscale::new_scale_color() 
+
+## mark species of interest
+pt_tree3 <- pt_tree2 +
+  ggtree::geom_tiplab(
+    mapping = aes(color = SpeciesName, label = nodeLabs),
+    size = 3, align = TRUE, linesize = 0.5
+  ) +
+  scale_x_continuous(expand = expansion(mult = c(0.05, 0.1))) +
+  scale_color_manual(
+    values = setNames(
+      c("red", "#E57A44", "#088734", "#088734"),
+      c(sampleInfoList[[outGroup]]$SpeciesName, "P. brasiliense",
+        "P. carotovorum", "P. c. subsp. carotovorum")
+    ),
+    breaks = NULL,
+    na.value = "black"
+  ) +
+  ggnewscale::new_scale_color()
+
+
+pt_tree4 <- annotate_ggtree(pt = pt_tree3, offset = 0.25)
+
+# pt_tree4
+
+ggsave(
+  plot = pt_tree4, width = 10, height = 20, scale = 2,
+  filename = paste(outDir, "/", opts$out, "_tree.pdf", sep = "")
+)
---
+++ scripts/notebooks/07_downstream_analysis.md
@@ -0,0 +1,97 @@
+# Downstream analysis of pangenome data
+
+## setup
+
+```bash
+#!/usr/bin/env bash
+
+shopt -s expand_aliases
+source ~/.bash_aliases
+
+# set -e
+# set -u
+set -o pipefail
+
+source $TOOLS_PATH/miniconda3/etc/profile.d/conda.sh
+conda activate pantools_master
+
+# PANGENOME_NAME=$1
+# PANGENOME_NAME='pectobacterium.10g'
+PANGENOME_NAME='pectobacterium.v2'
+
+## Setup
+PROJECT_DIR="/lustre/BIF/nobackup/$USER/projects/03_Pectobacterium"
+PANGENOME_DIR="$PROJECT_DIR/data/pangenomes/$PANGENOME_NAME"
+######################################################################
+```
+
+## Phylogeny
+
+### Generate various trees with default layout
+
+```bash
+## UPGMA tree
+./scripts/c_analysis/c02_explore_pangenome_phylogeny.R \
+--tree analysis/02_fastANI/ANI_UPGMA.newick \
+--name "ANI UPGMA" --out ANI_UPGMA_rooted --config project_config.yaml
+
+## Neighbor joining tree
+./scripts/c_analysis/c02_explore_pangenome_phylogeny.R \
+--tree analysis/02_fastANI/ANI_NJ.newick \
+--name "Neighbor Joining" --out ANI_NJ_rooted --config project_config.yaml
+
+## gene distance NJ tree
+./scripts/c_analysis/c02_explore_pangenome_phylogeny.R \
+--tree ./data/pangenomes/pectobacterium.v2/pectobacterium.v2.DB/gene_classification.100.0/gene_distance.tree \
+--name "gene distance NJ" --out gene_NJ_rooted --config project_config.yaml
+
+## kmer distance NJ tree
+./scripts/c_analysis/c02_explore_pangenome_phylogeny.R \
+--tree ./data/pangenomes/pectobacterium.v2/pectobacterium.v2.DB/kmer_classification.100.0/genome_kmer_distance.tree \
+--name "kmer distance NJ" --out kmer_NJ_rooted --config project_config.yaml
+
+
+```
+
+## PCR probe BLAST
+
+#### combine multiple genome FASTA into a single file
+
+```bash
+ANALYSIS_DIR="$PROJECT_DIR/analysis/03_pangenome_pecto_v2/insilico_assay"
+[ ! -d $ANALYSIS_DIR ] && mkdir $ANALYSIS_DIR
+
+## combine genomes and prepare blastn index
+python3 scripts/a_preprocessing/a10_merge_genome_faa.py --geno $PANGENOME_DIR/genomes_fa.list \
+--out $PANGENOME_DIR/blastdb/genomes_combined.fa
+
+## index the sequences
+makeblastdb -dbtype nucl -in $PANGENOME_DIR/blastdb/genomes_combined.fa \
+-title $PANGENOME_NAME -logfile $PANGENOME_DIR/blastdb/makeblastdb.log
+
+```
+
+#### run BLASTN
+
+```bash
+## run blastn in blastn-short mode
+blastn -db $PANGENOME_DIR/blastdb/genomes_combined.fa \
+-query data/other/pecto_assay/pectobacterium_assay_probes.fasta \
+-task blastn-short -max_target_seqs 2000 -num_threads 12 \
+-outfmt "6 qseqid qstart qend qlen sseqid sstart send sstrand slen pident length mismatch qcovs gapopen evalue bitscore" \
+-out $ANALYSIS_DIR/probe_blastn.out
+
+```
+
+#### parse & summarize blastn result table
+
+```bash
+Rscript scripts/c_analysis/c02_blastn_parse.R
+
+######################################################################
+```
+
+### UML 
+
+![Class Diagram](http://www.plantuml.com/plantuml/proxy?src=https://raw.githubusercontent.com/Zingam/Markdown-Document-UML-Use-Test/master/UML/Instance.puml)
+
