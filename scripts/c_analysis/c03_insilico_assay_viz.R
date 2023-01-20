c7dcd9c5faacd1163eee6aa1a94777ce824bbee8
Pardeshi, Lakhansing <lakhansing.pardeshi@wur.nl>
2023-01-20 11:46:54 +0100

    parse and viz assay blast output

---
+++ scripts/c_analysis/c03_insilico_assay_viz.R
@@ -0,0 +1,396 @@
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(treeio))
+suppressPackageStartupMessages(library(ggtree))
+suppressPackageStartupMessages(library(GenomicRanges))
+
+## parse PCR primer blastn results and generate summary figure
+
+rm(list = ls())
+
+
+source("https://raw.githubusercontent.com/lakhanp1/omics_utils/main/01_RScripts/02_R_utils.R")
+################################################################################
+set.seed(124)
+
+confs <- prefix_config_paths(
+  conf = suppressWarnings(configr::read.config(file = "project_config.yaml")),
+  dir = "."
+)
+
+pangenome <- confs$data$pangenomes$pectobacterium.v2$name
+outDir <- confs$analysis$phylogeny$dir
+genus <- confs$genus
+outGroup <- confs$analysis$phylogeny$outgroup
+
+analysisName <- "bioassay_probe_match"
+
+outDir <- confs$analysis$insilico_assay$dir
+outPrefix <- file.path(outDir, analysisName)
+
+################################################################################
+
+genomeChrs <- suppressMessages(
+  readr::read_tsv(
+    file = confs$data$pangenomes[[pangenome]]$files$genome_chrs,
+    col_names = c("Genome", "chr"), col_types = "c"
+  )
+)
+
+genusPattern <- paste("(", genus, " )(?!sp\\.)", sep = "")
+
+sampleInfo <- suppressMessages(
+  readr::read_csv(file = confs$data$pangenomes[[pangenome]]$files$metadata)
+) %>% 
+  dplyr::mutate(
+    Genome = as.character(Genome),
+    SpeciesName = stringi::stri_replace(
+      str = SpeciesName, regex = genusPattern, replacement = "P. "
+    ),
+    SpeciesName = stringi::stri_replace(
+      str = SpeciesName, regex = "((\\w)[^ ]+ )((\\w)[^ ]+ )(subsp\\..*)",
+      replacement = "$2. $4. $5"
+    ),
+    virulence_pcr = dplyr::case_when(
+      virulence_pcr == "positive" ~ "+",
+      virulence_pcr == "negative" ~ "-",
+      TRUE ~ virulence_pcr
+    ),
+    nodeLabs = stringr::str_c(sampleName, " (", SpeciesName,")", sep = "")
+  ) %>% 
+  dplyr::select(sampleId, everything())
+
+sampleInfoList <- dplyr::select(
+  sampleInfo, sampleId, sampleName, SpeciesName, strain, nodeLabs, Genome
+) %>% 
+  purrr::transpose() %>% 
+  purrr::set_names(nm = purrr::map(., "sampleId"))
+
+probeInfo <- suppressMessages(readr::read_tsv(confs$data$other$files$probe_info)) %>% 
+  dplyr::mutate(
+    type = forcats::fct_relevel(.f = type, "forward", "probe", "reverse")
+  )
+
+################################################################################
+## extract the ANI tree information for the node order
+rawTree <- ape::read.tree(file = confs$analysis$phylogeny$files$tree_ani_upgma)
+
+## set negative length edges => 0
+rawTree$edge.length[rawTree$edge.length < 0] <- 0
+
+
+## P. brasiliense species clade
+nodeOfInterest <- dplyr::filter(sampleInfo, SpeciesName == "P. brasiliense") %>% 
+  dplyr::pull(Genome)
+
+clade <- ape::getMRCA(phy = rawTree, tip = nodeOfInterest)
+subTree <- ape::extract.clade(phy = rawTree, node = clade)
+
+sampleInfo <- dplyr::left_join(
+  x = tibble::tibble(Genome = nodeOfInterest),
+  y = sampleInfo, by = "Genome"
+)
+
+## tree for plotting 
+## add data to tree
+treeTbl <- as_tibble(subTree) %>%
+  dplyr::full_join(y = sampleInfo, by = c("label" = "Genome")) %>%
+  treeio::as.treedata()
+
+pt_treeMain <- ggtree::ggtree(
+  tr = treeTbl
+)
+# scale_x_continuous(
+#   expand = expansion(add = c(0.01, 0.06))
+# ) 
+# ggtree::geom_tiplab(
+#   mapping = aes(color = SpeciesName, label = nodeLabs),
+#   align = TRUE
+# ) +
+# ggtree::geom_treescale(x = 0, y = length(treeTbl@phylo$tip.label)-2) 
+
+leafOrder <- dplyr::arrange(.data = pt_treeMain$data, y) %>% 
+  dplyr::filter(isTip) %>% 
+  dplyr::pull(label)
+
+sampleInfo %<>% 
+  dplyr::mutate(
+    Genome = forcats::fct_relevel(Genome, !!!leafOrder)
+  )
+
+################################################################################
+## process assay blast result
+blastRes <- suppressMessages(
+  readr::read_tsv(
+    file = confs$analysis$insilico_assay$files$blastn,
+    col_names = c(
+      "qseqid", "qstart", "qend", "qlen", "sseqid", "sstart", "send", "sstrand", "slen",
+      "pident", "length", "mismatch", "qcovs", "gapopen", "evalue", "bitscore"
+    )
+  )
+) %>% 
+  dplyr::mutate(
+    sstrand = dplyr::case_when(
+      sstrand == "plus" ~ "+",
+      sstrand == "minus" ~ "-",
+    )
+  )
+
+
+blastGr <- GenomicRanges::makeGRangesFromDataFrame(
+  df = dplyr::mutate(
+    .data = blastRes,
+    chr = sseqid,
+    start = dplyr::if_else(sstrand == "+", true = sstart, false = send),
+    end = dplyr::if_else(sstrand == "+", true = send, false = sstart),
+    strand = sstrand
+  ) %>% 
+    dplyr::select(chr, start, end, strand, everything()),
+  keep.extra.columns = TRUE
+)
+
+## extend by 100bp and merge to generate the broad GRanges covering all PCR regions (F, P, R)
+reducedGr <- GenomicRanges::reduce(
+  x = blastGr, min.gapwidth = 100, ignore.strand = TRUE
+)
+
+mcols(reducedGr)$regionId <- paste("region_", 1:length(reducedGr), sep = "")
+
+ovlpHits <- GenomicRanges::findOverlaps(
+  query = blastGr, subject = reducedGr
+)
+
+if(any(duplicated(ovlpHits@from))){
+  stop("Duplicated hits found in GRanges findOverlaps()")
+}
+
+mcols(blastGr)$regionId <- NA
+mcols(blastGr)$regionId[ovlpHits@from] <- mcols(reducedGr)$regionId[ovlpHits@to]
+
+assayBlastDf <- dplyr::left_join(
+  x =  tibble::as_tibble(mcols(blastGr)), y = genomeChrs, by = c("sseqid" = "chr")
+) %>% 
+  # dplyr::left_join(
+  #   y = sampleInfo, by = "Genome"
+  # ) %>% 
+  dplyr::left_join(
+    y = probeInfo, by = c("qseqid" = "probeId")
+  ) %>% 
+  dplyr::group_by(Genome, assay, sseqid, regionId) %>% 
+  dplyr::mutate(
+    assayRegionHits = n(),
+    assayGenomeGrp = cur_group_id(),
+    assayMismatches = sum(mismatch)
+  ) %>%
+  dplyr::ungroup() %>% 
+  dplyr::arrange(Genome, assay, sseqid, sstart, type) %>% 
+  dplyr::select(Genome,
+                # sampleId, SpeciesName, strain,
+                assay, type,
+                assayRegionHits, regionId, assayGenomeGrp, everything())
+
+
+readr::write_tsv(x = assayBlastDf, file = paste(outPrefix, ".data.tab", sep = ""))
+
+## select only best matching assay
+bestAssaysInGenomes <- dplyr::filter(assayBlastDf, assay != "P_wasabiae_WPP163") %>% 
+  dplyr::distinct(
+    assayGenomeGrp, Genome, assay, sseqid, regionId, assayRegionHits, assayMismatches
+  ) %>% 
+  dplyr::group_by(Genome, assay) %>% 
+  dplyr::arrange(desc(assayRegionHits), assayMismatches, .by_group = TRUE) %>% 
+  dplyr::slice(1L) %>% 
+  dplyr::ungroup()
+
+bestAssaysInGenomes <- dplyr::select(bestAssaysInGenomes, assayGenomeGrp) %>% 
+  dplyr::left_join(y = assayBlastDf, by = "assayGenomeGrp") %>% 
+  dplyr::filter(assayRegionHits == 3) %>% 
+  dplyr::mutate(
+    primerCov = if_else(condition = qcovs == 100, true = "complete", false = "incomplete"),
+    # mismatch = if_else(condition = mismatch == 0, true = "no", false = "yes"),
+    primerCov = forcats::as_factor(primerCov)
+  )
+
+## finally select assays only for the Genomes in sampleInfo
+genomeAssayDetails <- dplyr::select(sampleInfo, Genome) %>% 
+  dplyr::left_join(y = bestAssaysInGenomes, by = "Genome") %>% 
+  dplyr::filter(!is.na(assay)) %>% 
+  dplyr::mutate(
+    Genome = forcats::fct_relevel(Genome, !!!leafOrder),
+    assay = forcats::fct_relevel(
+      assay, "Erwinia", "P_br_1360500", "LZI", "TIR"
+    )
+  )
+
+genomeAssaySummary <- dplyr::group_by(
+  bestAssaysInGenomes, assayGenomeGrp, Genome, assay, assayMismatches
+) %>% 
+  dplyr::summarise(
+    assayLen = sum(qlen), .groups = "drop"
+  ) %>% 
+  dplyr::mutate(
+    assayPident = 100*(assayLen - assayMismatches)/assayLen,
+    ####************************************************************************
+    ####*This calculation is not correct
+    ####*Need to use the original assay length from FASTA file
+    ####*Because, if `dplyr::filter(assayRegionHits == 3)` is changed to 
+    ####*include assays where 2 of F,P,R mapped, assayLen calculation will be
+    ####*wrong as it will exclude the missing probe length.
+    ####*Solution: update the probe metadata file to include the length too.
+    ####************************************************************************
+    
+    assayResult = if_else(assayMismatches == 0, "pass", "fail")
+  )
+
+################################################################################
+
+pt_tree <- pt_treeMain
+
+pt_pcrBlast <- ggplot2::ggplot(
+  data = genomeAssayDetails,
+  mapping = aes(x = type, y = Genome)
+) +
+  geom_tile(
+    mapping = aes(fill = pident, width = qcovs/100, height = qcovs/100),
+    color = "black", size = 0.5) +
+  labs(
+    title = "blastn results for forward (F), probe (P) and reverse (R) PCR primers"
+  ) +
+  facet_grid(cols = vars(assay), scales = "free_y") +
+  viridis::scale_fill_viridis(name = "% identity", option = "plasma") +
+  # scale_size_manual(
+  ### @size = primerCov,
+  #   name = "Primer coverage",
+  #   values = c("complete" = 1, "incomplete" = 0.25)
+  # ) +
+  scale_x_discrete(
+    labels = c("forward" = "F", "probe" = "P", "reverse" = "R")
+  ) +
+  guides(
+    size = guide_legend(override.aes = list(fill = "white"))
+  ) +
+  theme_bw() +
+  theme(
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.x = element_text(size = 14, face = "bold"),
+    plot.title = element_text(hjust = 0.5, vjust = 0),
+    strip.text.x = element_text(size = 12, face = "bold") 
+  )
+
+# pt_pcrBlast
+##################################################################################
+## generate annotation plots
+pt_theme <- theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 90, face = "bold", vjust = 0.5),
+    axis.text.y = element_blank(),
+    axis.title = element_blank(),
+    panel.grid = element_blank(),
+    axis.ticks.y = element_blank()
+  )
+
+pt_source <- dplyr::select(sampleInfo, Genome, source) %>% 
+  ggplot2::ggplot(
+    mapping = aes(x = "source", y = Genome, color = source)
+  ) +
+  geom_point(shape = 15, size = 1) +
+  scale_color_manual(
+    values = c("NCBI" = alpha("#406495",0), "WUR" = "#468e30", "NVWA" = alpha("#ff6600",0)),
+    breaks = NULL
+  ) +
+  pt_theme +
+  theme(
+    axis.ticks = element_blank()
+  )
+  
+pt_region <- dplyr::select(sampleInfo, Genome, geo_loc_country) %>% 
+  ggplot2::ggplot(
+    mapping = aes(x = "country", y = Genome, color = geo_loc_country)
+  ) +
+  geom_point(size = 1) +
+  scale_color_manual(
+    values = c("Netherlands" = "blue")
+  ) +
+  pt_theme +
+  theme(
+    axis.ticks = element_blank()
+  )
+
+pt_vir <- dplyr::select(sampleInfo, Genome, virulence) %>% 
+  dplyr::mutate(
+    virulence = forcats::fct_relevel(virulence, "virulent", "avirulent")
+  ) %>% 
+  ggplot2::ggplot(
+    mapping = aes(x = "virulence", y = Genome, shape = virulence, color = virulence)
+  ) +
+  geom_point(size = 1) +
+  scale_shape_manual(
+    name = "Virulence",
+    values = c("virulent" = 17, "avirulent" = 19)
+  ) +
+  scale_color_manual(
+    name = "Virulence",
+    values = c("virulent" = "red", "avirulent" = "green")
+  ) +
+  pt_theme +
+  theme(
+    axis.ticks = element_blank()
+  )
+
+
+pt_pcr <- dplyr::select(sampleInfo, Genome, virulence_pcr) %>% 
+  ggplot2::ggplot(
+    mapping = aes(x = "PCR", y = Genome, label = virulence_pcr, color = virulence_pcr)
+  ) +
+  geom_text(size = 4, fontface = "bold") +
+  scale_color_manual(
+    values = c("+" = "red", "-" = "green"), breaks = NULL
+  ) +
+  pt_theme +
+  theme(
+    axis.ticks = element_blank()
+  )
+
+pt_sampleYear <- dplyr::select(sampleInfo, Genome, collection_year, source) %>%
+  ggplot2::ggplot(
+    mapping = aes(x = collection_year, y = Genome, color = source)
+  ) +
+  geom_point(size = 1) +
+  scale_color_manual(
+    values = c("NCBI" = "black", "WUR" = "#468e30", "NVWA" = "black"),
+    breaks = NULL
+  ) +
+  scale_x_continuous(expand = expansion(add = 0.5)) +
+  coord_cartesian(xlim = c(2000, 2022)) +
+  labs(title = "Sampling\nyear") +
+  pt_theme +
+  theme(
+    plot.title = element_text(angle = 0, hjust = 0.5, vjust = 0)
+  )
+
+
+## arrange plots one by one
+pt_all <- pt_source %>% aplot::insert_left(pt_tree, width = 12) %>%
+  aplot::insert_right(pt_sampleYear, width = 8) %>%
+  aplot::insert_right(pt_region, width = 1) %>%
+  aplot::insert_right(pt_vir, width = 1) %>%
+  aplot::insert_right(pt_pcr, width = 0.5) %>%
+  aplot::insert_right(pt_pcrBlast, width = 15)
+
+
+png(filename = paste(outPrefix, ".png", sep = ""), width = 3000, height = 1500, res = 200)
+pt_all
+dev.off()
+
+
+pt_treeBlast <- pt_vir %>% aplot::insert_right(pt_tree, width = 15) %>%
+  aplot::insert_right(pt_pcrBlast, width = 15)
+
+
+png(filename = paste(outPrefix, ".treeBlast.png", sep = ""), width = 2500, height = 1500, res = 150)
+pt_treeBlast
+dev.off()
+
--- scripts/notebooks/07_downstream_analysis.md
+++ scripts/notebooks/07_downstream_analysis.md
@@ -58,7 +58,7 @@ PANGENOME_DIR="$PROJECT_DIR/data/pangenomes/$PANGENOME_NAME"
 #### combine multiple genome FASTA into a single file
 
 ```bash
-ANALYSIS_DIR="$PROJECT_DIR/analysis/03_pangenome_pecto_v2/insilico_assay"
+ANALYSIS_DIR="$PROJECT_DIR/analysis/04_pangenome_pecto_v2/insilico_assay"
 [ ! -d $ANALYSIS_DIR ] && mkdir $ANALYSIS_DIR
 
 ## combine genomes and prepare blastn index
@@ -66,20 +66,39 @@ python3 scripts/a_preprocessing/a10_merge_genome_faa.py --geno $PANGENOME_DIR/ge
 --out $PANGENOME_DIR/blastdb/genomes_combined.fa
 
 ## index the sequences
-makeblastdb -dbtype nucl -in $PANGENOME_DIR/blastdb/genomes_combined.fa \
+makeblastdb -dbtype nucl -in $PANGENOME_DIR/blastdb/genomes_combined.fa -parse_seqids \
 -title $PANGENOME_NAME -logfile $PANGENOME_DIR/blastdb/makeblastdb.log
 
 ```
 
 #### run BLASTN
 
+`blastn` was not showing hits of some primer sequences when searched against all the genomes.
+Hence, database sequences are split into batches of 100 genomes and `blastdb_aliastool` is 
+run to preprocess the sequence ids. These preprocessed sequence ids are provided
+as input to `-seqidlist ` argument while running `blastn`. Somehow, when the search
+space is limited using `-seqidlist` option, missing hits are shown. This is run for
+each set of sequence ids.
+
 ```bash
-## run blastn in blastn-short mode
-blastn -db $PANGENOME_DIR/blastdb/genomes_combined.fa \
--query data/other/pecto_assay/pectobacterium_assay_probes.fasta \
--task blastn-short -max_target_seqs 2000 -num_threads 12 \
--outfmt "6 qseqid qstart qend qlen sseqid sstart send sstrand slen pident length mismatch qcovs gapopen evalue bitscore" \
--out $ANALYSIS_DIR/probe_blastn.out
+
+for chrset in $PANGENOME_DIR/blastdb/chrset_*.acc
+do
+    ##
+    blastdb_aliastool -seqid_file_in ${chrset}
+
+    setName=`basename ${chrset} | sed 's/.acc//'`
+    blastnOut="probe_blastn.${setName}.out"
+
+    ## run blastn in blastn-short mode
+    blastn -db $PANGENOME_DIR/blastdb/genomes_combined.fa \
+    -query data/other/pecto_assay/pectobacterium_assay_probes.fasta \
+    -task blastn-short -num_threads 12 -max_target_seqs 5000 -seqidlist ${chrset}.bsl \
+    -outfmt "6 qseqid qstart qend qlen sseqid sstart send sstrand slen pident length mismatch qcovs gapopen evalue bitscore" \
+    -out $ANALYSIS_DIR/${blastnOut}
+
+done
+
 
 ```
 
