dda67e44bfb6b4cb5c1fc0dab216eb536b5e9de0
Lakhansing Pardeshi <lakhanp1@gmail.com>
2023-02-28 17:41:07 +0100

    construction pipeline update

--- scripts/b_construction/build_pangenome.sh
+++ scripts/b_construction/build_pangenome.sh
@@ -41,7 +41,7 @@ PANGENOME_NAME=$1
 source $TOOLS_PATH/miniconda3/etc/profile.d/conda.sh
 conda activate pantools_master
 
-export PANTOOLS="$PANTOOLS_4_1"
+export PANTOOLS="$PANTOOLS_DEV"
 
 ## Setup
 PROJECT_DIR="/lustre/BIF/nobackup/$USER/projects/03_Pectobacterium"
@@ -49,8 +49,8 @@ PANGENOME_DIR="$PROJECT_DIR/data/pangenomes/$PANGENOME_NAME"
 
 ## setup for local disk processing on handelsman
 LOCAL_DIR_PATH="/local_scratch/$USER"
-# LOCAL_DIR_PATH="/local/$USER"
-# LOCAL_DIR_PATH="/dev/shm/$USER"
+#LOCAL_DIR_PATH="/local/$USER"
+#LOCAL_DIR_PATH="/dev/shm/$USER"
 
 PAN_BUILD_DIR="$LOCAL_DIR_PATH/03_Pectobacterium"
 pan_db="$PAN_BUILD_DIR/${PANGENOME_NAME}.DB${DB_SUFFIX}"
@@ -75,75 +75,78 @@ fi
 
 ######################################################################
 
-#TMPDIR="/local/$USER/tmp"
+#TMPDIR="$LOCAL_DIR_PATH/tmp"
 #[ ! -d ${TMPDIR} ] && mkdir -p ${TMPDIR}
 #[ -d ${TMPDIR}/pantools ] && rm -rd ${TMPDIR}/pantools
 #[ ! -d ${TMPDIR}/pantools ] && mkdir -p ${TMPDIR}/pantools
-#[ -d ${TMPDIR}/spark ] && rm -rd ${TMPDIR}/spark
-#[ ! -d ${TMPDIR}/spark ] && mkdir -p ${TMPDIR}/spark
 
-#cache=25000000
-#PANTOOLS_LOCALIZATION_OUTPUT=${TMPDIR}/pantools
-#SPARK_LOCAL_DIRS=${TMPDIR}/spark
-#PANTOOLS_ADDRESS_CACHE_SIZE=${cache}
-#PANTOOLS_NUM_DB_WRITER_THREADS=10
-
-### Construct pangenome using build_pangenome_parallel
-#process_start build_pangenome_parallel
-#$PANTOOLS_OPT build_pangenome_parallel --database-path ${pan_db} \
-#--genomes-file $PANGENOME_DIR/genomes_fa.list -tn 40
+#process_start build_pangenome
+#$PANTOOLS build_pangenome --threads 40 --scratch-directory ${TMPDIR}/pantools \
+#--num-db-writer-threads 10 --cache-size 25000000 \
+#${pan_db} $PANGENOME_DIR/genomes_fa.list
 #error_exit $?
 
 #cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.raw
 
 #######################################################################
-## switch back to main version
-export PANTOOLS="$PANTOOLS_DEV"
-## add annotations
-process_start add_annotations
-$PANTOOLS add_annotations --connect ${pan_db} $PANGENOME_DIR/genomes_gff3.list
-error_exit $?
+### add annotations
+#process_start add_annotations
+#$PANTOOLS add_annotations --connect ${pan_db} $PANGENOME_DIR/genomes_gff3.list
+#error_exit $?
 
-cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.ann
+## add phenotypes
+#process_start add_phenotypes
+#printf "y\n" | $PANTOOLS remove_phenotypes ${pan_db}
+#$PANTOOLS remove_phenotypes ${pan_db}
+#$PANTOOLS add_phenotypes ${pan_db} $PANGENOME_DIR/genomes_metadata.csv
+#error_exit $?
 
-# add phenotypes
-process_start add_phenotypes
-printf "y\n" | $PANTOOLS remove_phenotypes ${pan_db}
-$PANTOOLS remove_phenotypes ${pan_db}
-$PANTOOLS add_phenotypes ${pan_db} $PANGENOME_DIR/genomes_metadata.csv
-error_exit $?
+### add_functions
+#process_start add_InterProScan_annotations
+#$PANTOOLS remove_functions ${pan_db}
+#$PANTOOLS add_functions ${pan_db} $PANGENOME_DIR/functional_annotations.txt
+#error_exit $?
 
-cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.meta
 
-## add_functions
-process_start add_InterProScan_annotations
-$PANTOOLS remove_functions ${pan_db}
-$PANTOOLS add_functions ${pan_db} $PANGENOME_DIR/functional_annotations.txt
+## BUSCO
+process_start busco_protein
+$PANTOOLS busco_protein -t 30 --busco10 enterobacterales_odb10 ${pan_db}
 error_exit $?
 
 cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 
-## BUSCO
-#process_start busco_protein
-#$PANTOOLS busco_protein -t 30 --busco10 enterobacterales_odb10 ${pan_db}
-#error_exit $?
+######################################################################
+## type strains
+#bash ./scripts/b_construction/grouping_subsets_process.sh "typeStrain" "374,96,385,386,379,388,116,375,377,347,250,335,338,265,256,32,269,387,266"
 
-#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.backup_bsk
+### generate random subsets to run `optimal_grouping`
+#Rscript scripts/c_analysis/grouping_subsets_build.R
 
-## BUSCO multiqc
-#process_start 
-#multiqc --filename multiqc_busco -outdir analysis/01_multiqc --ignore "run_enterobacterales_odb10" --module busco -d -dd 1 ${pan_db}/busco/enterobacterales_odb10/protein/results/
-#error_exit $?
+### run `optimal_grouping` on these random subsets
+#nohup \
+## sed -n '1,6!p' analysis/03_pangenome_pecto_v2/subset_optimal_group/subsets_conf.tab | \
+#cat analysis/03_pangenome_pecto_v2/subset_optimal_group/subsets_conf.tab | \
+#parallel --colsep '\t' --jobs 10  --keep-order --workdir $PWD --halt soon,fail=1 \
+#--load 100% --results logs/pantools/sub_opt_group/{1} \
+#--joblog logs/pantools/sub_opt_group/sub_opt_group.log \
+#bash ./scripts/b_construction/grouping_subsets_process.sh {1} {2} \
+#>>logs/pantools/sub_opt_group/nohup.out 2>&1 &
+
+### summarize the results
+#Rscript scripts/c_analysis/grouping_subsets_analyze.R
+
+######################################################################
+## grouping with relaxation 4 setting
+process_start group_v4
+nice $PANTOOLS group -t 30 --relaxation 4 ${pan_db}
+error_exit $?
+
+cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.grp
 
 ## optimized grmultiqc_buscoouping
 #process_start optimal_grouping
-#$PANTOOLS optimal_grouping -t 30 ${pan_db} ${pan_db}/busco/enterobacterales_odb10
-#$PANTOOLS optimal_grouping -dp ${pan_db} -if ${pan_db}/busco/enterobacterales_odb10 -tn 25
+#nice $PANTOOLS optimal_grouping -t 30 ${pan_db} ${pan_db}/busco/enterobacterales_odb10
 #error_exit $?
-
-#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.backup_grp
-
-#cp -r $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.backup_grp ${pan_db}
 
 #Rscript ${pan_db}/optimal_grouping/optimal_grouping.R
 #$PANTOOLS grouping_overview ${pan_db}
@@ -151,15 +154,13 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 #$PANTOOLS change_grouping -v 4 ${pan_db}
 #$PANTOOLS grouping_overview ${pan_db}
 
-#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.backup_grp
+#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.opt_grp
 
-##################
+######################################################################
 ### extract the metrics from pangenome
 #process_start extract_pangenome_metrics
 #$PANTOOLS metrics ${pan_db}
 #error_exit $?
-
-######################################################################
 
 ### Core unique thresholds
 #process_start core_unique_thresholds
@@ -174,8 +175,8 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 
 ### Gene distance tree
 #Rscript ${pan_db}/gene_classification/gene_distance_tree.R
-#mv ${pan_db}/gene_classification ${pan_db}/gene_classification.100.0
 
+#mv ${pan_db}/gene_classification ${pan_db}/gene_classification.100.0
 
 ### gene classification: soft core and cloud
 #process_start gene_classification_softcore_cloud
@@ -186,8 +187,14 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 #Rscript ${pan_db}/gene_classification/gene_distance_tree.R
 #mv ${pan_db}/gene_classification ${pan_db}/gene_classification.95.5
 
-#######################################################################
+### homology group information 
+#sed -i.bak -r -n '/^#/! p' ${pan_db}/gene_classification.100.0/all_homology_groups.csv
+#process_start "extracting homology group information"
+#$PANTOOLS group_info -H ${pan_db}/gene_classification.100.0/all_homology_groups.csv ${pan_db}
+#error_exit $?
 
+########################################################################
+#### k-mer classification
 ### K-mer classification: soft core and cloud
 #process_start kmer_classification_core_unique
 #$PANTOOLS k_mer_classification ${pan_db}
@@ -195,7 +202,6 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 
 #Rscript ${pan_db}/kmer_classification/genome_kmer_distance_tree.R
 #mv ${pan_db}/kmer_classification ${pan_db}/kmer_classification.100.0
-
 
 ### K-mer classification: soft core and cloud
 #process_start kmer_classification_softcore_cloud
@@ -205,8 +211,8 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 #Rscript ${pan_db}/kmer_classification/genome_kmer_distance_tree.R
 #mv ${pan_db}/kmer_classification ${pan_db}/kmer_classification.95.5
 
-#######################################################################
-
+########################################################################
+#### Pangenome structure
 ### Pangenome structure: genes
 #process_start pangenome_structure_gene
 #$PANTOOLS pangenome_structure -t 20 ${pan_db}
@@ -216,9 +222,9 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 #Rscript ${pan_db}/pangenome_size/gene/gains_losses_median_or_average.R
 #Rscript ${pan_db}/pangenome_size/gene/gains_losses_median_and_average.R
 #Rscript ${pan_db}/pangenome_size/gene/heaps_law.R
-#Rscript ${pan_db}/pangenome_size/gene/core_access_unique.R
 
-#running
+#mv ${pan_db}/pangenome_size/gene ${pan_db}/pangenome_size/gene.pangenome
+
 ### Pangenome structure: kmer
 #process_start pangenome_structure_kmer
 #$PANTOOLS pangenome_structure -t 20 --kmer ${pan_db}
@@ -226,15 +232,13 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 
 #Rscript ${pan_db}/pangenome_size/kmer/pangenome_growth.R
 
-######################################################################
-
+#######################################################################
 ### functional_classification: core and unique
 #process_start functional_classification_core_unique
 #$PANTOOLS functional_classification ${pan_db}
 #error_exit $?
 
 #mv ${pan_db}/function/functional_classification ${pan_db}/function/functional_classification.100.0
-
 
 ### functional_classification: softcore and cloud
 #process_start functional_classification_softcore_cloud
@@ -243,60 +247,32 @@ cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.fn
 
 #mv ${pan_db}/function/functional_classification ${pan_db}/function/functional_classification.95.5
 
-
 ### function_overview
 #process_start function_overview
 #$PANTOOLS function_overview ${pan_db} 
 #error_exit $?
 
-#Rscript ${pan_db}/cog_per_class.R
+##Rscript ${pan_db}/cog_per_class.R
 
+#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.chr
+#######################################################################
 
 ### MSA for homology groups
 #process_start "msa for homology groups"
-#$PANTOOLS msa -t 12 --method per-group --mode nucleotide ${pan_db}
+#$PANTOOLS msa -t 30 --method per-group --mode nucleotide ${pan_db}
 #error_exit $?
 
+#cp -r ${pan_db} $PANGENOME_DIR/backup/${PANGENOME_NAME}.DB.msa
 #######################################################################
 
 ### SNP tree using core gene SNPs
 #process_start core_phylogeny
 #cp -r ${pan_db}/gene_classification.100.0 ${pan_db}/gene_classification
-#$PANTOOLS core_phylogeny -t 20  --clustering-mode ML ${pan_db}
+#$PANTOOLS core_phylogeny -t 30  --clustering-mode ML ${pan_db}
 #error_exit $?
 
 #rm ${pan_db}//core_snp_tree/informative.fasta.*
-#iqtree -nt 20 -s ${pan_db}/core_snp_tree/informative.fasta -redo -bb 1000
-
-######################################################################
-
-### add the updated phenotypes for association analysis
-#process_start add_phenotypes
-#$PANTOOLS remove_phenotype ${pan_db}
-#$PANTOOLS add_phenotypes ${pan_db} analysis/04_pangenome_pecto_v2/phylogeny/clade_compare_phenotypes.csv
-#error_exit $?
-
-
-#[ -d ${pan_db}/gene_classification ] && rm -r ${pan_db}/gene_classification
-#mkdir ${pan_db}/gene_classification.pheno
-
-### Gene classification for each phenotype
-#phenotypes=(`awk -F "\t" '{ print $1 }' data/analysis_configs/pheno_association_config.tab`)
-#for phn in ${phenotypes[@]}
-#do
-#    process_start "gene_classification for phenotype $phn"
-#    pheno_arg=`grep "^${phn}\b" data/analysis_configs/pheno_association_config.tab | cut -f2`
-#    $PANTOOLS gene_classification ${pheno_arg} ${pan_db}
-#    error_exit $?
-
-#    ## move results to a folder
-#    pheno_dir=${pan_db}/gene_classification.pheno/${phn}
-#    [ -d ${pheno_dir} ] && rm -r ${pheno_dir}
-#    mkdir ${pheno_dir}
-#    mv ${pan_db}/gene_classification/{phenotype_*,gene_classification_phenotype_overview.txt} ${pheno_dir}/
-#done
-
-#rm -r ${pan_db}/gene_classification
+#iqtree -nt 30 -s ${pan_db}/core_snp_tree/informative.fasta -redo -bb 1000
 
 ######################################################################
 
