1c812d4a68f822396c4dd5f8e7934436aa10303b
Pardeshi, Lakhansing <lakhansing.pardeshi@wur.nl>
2023-11-13 12:10:39 +0100

    use phylo, dend and hclust for clustering in heatmap

--- scripts/utils/genome_comparison.R
+++ scripts/utils/genome_comparison.R
@@ -17,24 +17,41 @@ plot_species_ANI_heatmap <- function(mat, phy, speciesInfo = NULL, markGenomes =
   
   ## necessary checks
   stopifnot(
-    setequal(rownames(mat), phy$tip.label),
-    ## ensure the row order is same: this is because of a bug in ComplexHeatmap
-    all(rownames(mat) == phy$tip.label),
     setequal(rownames(mat), colnames(mat)),
-    any(class(phy) == "phylo")
+    any(isa(phy, c("phylo", "dendrogram", "hclust")))
   )
   
+  if (isa(phy, "phylo")) {
+    clust <- as.dendrogram_ordered.phylo(phy = phy, sourceOrder = rownames(mat))
+  } else if (isa(phy, "hclust")) {
+    clust <- as.dendrogram(phy)
+  } else {
+    clust <- phy
+  }
   
-  ## ANI heatmap
-  ht_ani <- ComplexHeatmap::Heatmap(
+  stopifnot(
+    # all(rownames(mat) == labels(clust)),
+    setequal(rownames(mat), labels(clust))
+  )
+  
+  ht_args <- list(
     matrix = mat,
-    cluster_rows = ape::as.hclust.phylo(phy), row_dend_reorder = FALSE,
-    cluster_columns = ape::as.hclust.phylo(phy), column_dend_reorder = FALSE,
+    cluster_rows = clust,
+    cluster_columns = clust,
     show_row_dend = TRUE, show_column_dend = FALSE,
     row_dend_width = unit(3, "cm"),
     show_row_names = FALSE, show_column_names = FALSE,
-    column_title = "similarity scores",
     ...
+  )
+  
+  ht_args$column_title <- ifelse(
+    is.null(ht_args$column_title), "similarity scores", ht_args$column_title
+  )
+  
+  ## ANI heatmap
+  ht_ani <- do.call(
+    what = ComplexHeatmap::Heatmap,
+    args = ht_args
   )
   
   htList <- ht_ani
@@ -59,4 +76,42 @@ plot_species_ANI_heatmap <- function(mat, phy, speciesInfo = NULL, markGenomes =
   
 }
 
+################################################################################
+
+#' Correct the order of the dendrogram created from the `phylo` object
+#' 
+#' This function fixes the ComplexHeatmap issue #305. 
+#' https://github.com/jokergoo/ComplexHeatmap/issues/305
+#'
+#' @param phy a `phylo` object
+#' @param sourceOrder a `rownames()` or `colnames()` vector from the source
+#' matrix to which the `order.dendrogram()` need to match
+#'
+#' @return A dendrogram object with corrected order
+#' @export
+#'
+#' @examples NA
+as.dendrogram_ordered.phylo <- function(phy, sourceOrder){
+  
+  stopifnot(
+    isa(phy, "phylo"),
+    setequal(sourceOrder, phy$tip.label)
+  )
+  
+  dnd <- as.dendrogram(ape::as.hclust.phylo(phy))
+  
+  # handle phylo->dendrogram object in ComplexHeatmap: 
+  # fix the order.dendrogram() to correctly map to the matrix rows and columns
+  # https://github.com/jokergoo/ComplexHeatmap/issues/305
+  
+  # head(order.dendrogram(dnd))
+  # head(labels(dnd))
+  # sourceOrder[head(order.dendrogram(dnd))]
+  
+  phyloOrder <- structure(1:ape::Ntip(phy), names = sourceOrder)
+  newOrder <- unname(phyloOrder[labels(dnd)])
+  dendextend::order.dendrogram(dnd) <- newOrder
+  
+  return(dnd)
+}
 ################################################################################
