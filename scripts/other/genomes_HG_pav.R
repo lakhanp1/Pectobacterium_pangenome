9536e4241e77ab7529ce21ebcafb9801e5a910ff
Pardeshi, Lakhansing <lakhansing.pardeshi@wur.nl>
2024-02-16 23:06:09 +0100

    add gene position, broad function, pangenome class etc

--- scripts/other/genomes_HG_pav.R
+++ scripts/other/genomes_HG_pav.R
@@ -5,35 +5,133 @@ suppressMessages(library(tidyverse))
 
 rm(list = ls())
 
+source("https://raw.githubusercontent.com/lakhanp1/omics_utils/main/RScripts/utils.R")
+source("scripts/utils/config_functions.R")
 source("scripts/utils/homology_groups.R")
 
+################################################################################
+
+genomeOfInterest <- c("g_187", "g_399", "g_179", "g_191")
+coreClade <- "n11"
+nodeExclude <- "n22"
+
+treeMethod <- "kmer_upgma" # ani_upgma, kmer_upgma
 panOrgDb <- org.Pectobacterium.spp.pan.eg.db
 
-# genomes <- c("g_418", "g_162", "g_172")
-genomes <- c("g_187", "g_399", "g_179", "g_191")
+confs <- prefix_config_paths(
+  conf = suppressWarnings(configr::read.config(file = "project_config.yaml")),
+  dir = "."
+)
 
-subPanHgs <- sub_pangenome_hgs(pandb = panOrgDb, genomes = genomes)
+column_nodepath <- paste("nodepath.", treeMethod, sep = "")
+pangenome <- confs$data$pangenomes$pectobacterium.v2$name
+panConf <- confs$data$pangenomes[[pangenome]]
+################################################################################
 
-df <- AnnotationDbi::select(
-  panOrgDb, keys = genomes,
+sampleInfo <- get_metadata(file = panConf$files$metadata, genus = confs$genus)
+
+subGenomes <- dplyr::filter(
+  sampleInfo,
+  stringr::str_detect(
+    string = !!sym(column_nodepath), pattern = paste(";", coreClade, ";", sep = "")
+  )
+)
+
+subGenomes <- dplyr::filter(
+  sampleInfo,
+  stringr::str_detect(
+    string = !!sym(column_nodepath), pattern = paste(";", coreClade, ";", sep = "")
+  ),
+  stringr::str_detect(
+    string = !!sym(column_nodepath), pattern = paste(";", nodeExclude, ";", sep = ""),
+    negate = TRUE
+  )
+)
+
+################################################################################
+# get HG PAV for genomes under a node 
+subPanHgs <- sub_pangenome_hgs(
+  pandb = panOrgDb,
+  genomes = union(genomeOfInterest, subGenomes$genomeId)
+) %>% 
+  dplyr::rename(subset_class = class, subset_nGenomes = nGenomes)
+
+# HG PAV for pangenome
+panHgs <- sub_pangenome_hgs(
+  pandb = panOrgDb, genomes = sampleInfo$genomeId
+) %>% 
+  dplyr::rename(pan_class = class, pan_nGenomes = nGenomes)
+
+# HG PAV for genomes of interest
+genomeHgsPav <- sub_pangenome_hgs(pandb = panOrgDb, genomes = genomeOfInterest)
+
+# HG position
+hgPos <- AnnotationDbi::select(
+  panOrgDb, keys = genomeOfInterest,
+  columns = c("genomeId", "GID", "chr_id", "start", "end"),
+  keytype = "genomeId"
+) %>% 
+  dplyr::mutate(
+    dplyr::across(.cols = c(start, end), .fns = as.numeric),
+  ) %>% 
+  dplyr::group_by(genomeId) %>% 
+  dplyr::arrange(chr_id, start) %>% 
+  dplyr::mutate(hgPosition = 1:n()) %>% 
+  dplyr::ungroup() %>% 
+  tidyr::pivot_wider(
+    id_cols = c(GID),
+    names_from = genomeId, values_from = hgPosition,
+    values_fn = ~ min(.x)
+  ) %>% 
+  dplyr::select(hgId = GID, !!!genomeOfInterest) %>% 
+  tidyr::replace_na(
+    as.list(rep(-1, length(genomeOfInterest))) %>% purrr::set_names(genomeOfInterest)
+  ) %>% 
+  dplyr::arrange(!!!syms(genomeOfInterest))
+
+# annotation data for genomes of interest
+hgAn <- AnnotationDbi::select(
+  panOrgDb, keys = genomeOfInterest,
   columns = c("genomeId", "GID", "COG_description", "pfam_description"),
   keytype = "genomeId"
 ) %>%
   dplyr::group_by(GID) %>% 
-  dplyr::mutate(
+  dplyr::summarise(
     dplyr::across(
       .cols = c(COG_description, pfam_description),
       .fns = ~paste(unique(.x), collapse = "; ")
+    )
+  ) %>% 
+  tidyr::unite(
+    col = cog_pfam, COG_description, pfam_description, sep = "; ", remove = FALSE
+  ) %>% 
+  dplyr::mutate(
+    broad_function = stringr::str_extract_all(
+      string = cog_pfam,
+      pattern = regex(
+        "integrase|secretion.system|methyltransferase|restriction|integrase|transposase|toxin",
+        ignore_case = TRUE
+      )
     ),
-    value = 1
+    broad_function = purrr::map_chr(
+      .x = broad_function,
+      .f = function(x){
+        if(length(x) > 0){
+          return(paste(unique(tolower(x)), collapse = ";"))
+        } else {
+          return("")
+        }
+      }
+    )
   ) %>% 
-  dplyr::distinct() %>% 
-  dplyr::ungroup() %>% 
-  tidyr::pivot_wider(
-    names_from = genomeId, values_fill = 0
-  ) %>% 
-  dplyr::left_join(y = subPanHgs, by = c("GID" = "hgId")) %>% 
-  dplyr::arrange(desc(nGenomes)) %>% 
-  dplyr::select(GID, nGenomes, class, !!!genomes, everything())
+  dplyr::select(-cog_pfam) %>% 
+  dplyr::relocate(COG_description, pfam_description, .after = broad_function)
+
+# combine data
+df <- dplyr::left_join(x = hgPos, y = genomeHgsPav, by = "hgId") %>% 
+  dplyr::left_join(y = hgAn, by = c("hgId" = "GID")) %>% 
+  dplyr::left_join(y = panHgs, by = "hgId") %>% 
+  dplyr::left_join(y = subPanHgs, by = "hgId") 
 
 clipr::write_clip(df)
+
